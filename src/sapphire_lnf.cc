f27fcf5352ca2b0bc6bb89ea07f8bd792fd95e4b
Morgon Kanter <morgon.kanter@gmail.com>
2023-05-05 01:55:34 -0400

    Also draw the marker.

--- src/sapphire_lnf.cc
+++ src/sapphire_lnf.cc
@@ -1,3 +1,5 @@
+#include <cmath>
+
 #include "sapphire_lnf.h"
 
 using juce::degreesToRadians;
@@ -23,18 +25,22 @@ void LookAndFeel::drawRotarySlider(juce::Graphics &g, int x, int y, int width, i
         knob_cache_ =
             std::make_unique<juce::Image>(juce::Image::ARGB, width * sf, height * sf, true);
         juce::Graphics cg(*knob_cache_);
+        // Opacities taken from the SVG files, since Juce isn't smart enough to just use them, sigh.
         knob_->drawWithin(cg, juce::Rectangle{0, 0, width * sf, height * sf}.toFloat(),
                           juce::RectanglePlacement(), 1.f);
+        knob_marker_->drawWithin(cg, juce::Rectangle{0, 0, width * sf, height * sf}.toFloat(),
+                                 juce::RectanglePlacement(), 1.f);
         rotary_scale_factor_ = sf;
-        std::cout << "Scale factor: " << sf << std::endl;
     }
 
-    // g.drawImageAt(*knob_cache_, x, y);
+#if 1
     g.drawImage(*knob_cache_, x, y, width, height, 0, 0, width * sf, height * sf);
+#else
     // Maybe do this instead, and set to a buffered image at the component level.
-#if 0
-    knob_->drawWithin(g, juce::Rectangle{x, y, width, height}.toFloat(),
-                      juce::RectanglePlacement(), 1.f);
+    knob_->drawWithin(g, juce::Rectangle{x, y, width, height}.toFloat(), juce::RectanglePlacement(),
+                      1.f);
+    knob_marker_->drawWithin(g, juce::Rectangle{x, y, width, height}.toFloat(),
+                             juce::RectanglePlacement(), 1.f);
 #endif
 }
 
