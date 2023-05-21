2969cc6e2c61bc53c036bf28be794b2b55b7b2bf
Morgon Kanter <morgon.kanter@gmail.com>
2023-05-21 02:10:23 -0400

    Create actual variables for all the controls of interest.

--- src/ElastikaEditor.cpp
+++ src/ElastikaEditor.cpp
@@ -1,8 +1,33 @@
+#include <exception>
+#include <unordered_map>
+#include <utility>
+
 #include "ElastikaProcessor.h"
 #include "ElastikaEditor.h"
 #include "ElastikaBinary.h"
 #include "led_vu.h"
 #include "sapphire_lnf.h"
+
+namespace
+{
+
+// Helper function for placing a control exactly. Juce positioning doesn't take subpixel locations,
+// unlike the rest of Juce. So apply a rounded version of that and add a transform that shunts it to
+// the exact right location.
+//
+// cx and cy are the coordinates from the SVG. These are, for whatever reason, slightly off when
+// used on top of our background. dx and dy are the correction for that off-ness (determined
+// experimentally).
+void set_control_position(juce::Component &control, float cx, float cy, float dx, float dy)
+{
+    juce::Point<float> real{cx + dx, cy + dy};
+    juce::Point<int> rounded = real.toInt();
+    control.setCentrePosition(rounded);
+    control.setTransform(juce::AffineTransform::translation(real.getX() - rounded.getX(),
+                                                            real.getY() - rounded.getY()));
+}
+
+} // namespace
 
 ElastikaEditor::ElastikaEditor(ElastikaAudioProcessor &p)
     : juce::AudioProcessorEditor(&p), processor(p)
@@ -28,86 +53,90 @@ ElastikaEditor::ElastikaEditor(ElastikaAudioProcessor &p)
     background->setInterceptsMouseClicks(false, true);
     addAndMakeVisible(*background);
 
-    //  Find knobs.
-    std::cout << "Top tag name: " << xml->getTagName() << std::endl;
+    // Find controls.
     auto controls = xml->getChildByAttribute("id", "ControlLayer");
     if (!controls)
     {
-        std::cout << "Big failure!" << std::endl;
-        return;
+        throw std::out_of_range("Missing ControlLayer in SVG");
     }
+    std::unordered_map<std::string, std::pair<float, float>> found;
     for (const auto *control : controls->getChildWithTagNameIterator("circle"))
     {
-        std::cout << "id: " << control->getStringAttribute("id")
-                  << "; x: " << control->getStringAttribute("cx")
-                  << "; y: " << control->getStringAttribute("cy") << std::endl;
         const juce::String &id = control->getStringAttribute("id");
-        // Offsets from declared controls in the SVG. Determined through extensive trial and error.
-        float dx = 0.5f; // was 1.05f when sliders were knobs by mistake
-        float dy = 0.5f; // Seems to be based on the knob size (11; compare knob size 6)
         float cx = control->getStringAttribute("cx").getFloatValue();
         float cy = control->getStringAttribute("cy").getFloatValue();
-        if (id.endsWith("knob") || id.endsWith("atten"))
-        {
-            auto kn = std::make_unique<juce::Slider>();
-            kn->setSliderStyle(juce::Slider::RotaryHorizontalVerticalDrag);
-            kn->setTextBoxStyle(juce::Slider::NoTextBox, true, 0, 0);
-            kn->setPopupMenuEnabled(true);
-            background->addAndMakeVisible(*kn);
-            if (id.endsWith("knob"))
-            {
-                kn->setSize(11, 11);
-                dx = 0.5f;
-                dy = 0.5f;
-            }
-            else if (id.endsWith("atten"))
-            {
-                kn->setLookAndFeel(small_lnf.get());
-                kn->setSize(6, 6);
-                dx = 0.9166f;
-                dy = 0.9166f;
-            }
-            elements.push_back(std::move(kn));
-        }
-        else if (id.endsWith("slider"))
-        {
-            auto sl = std::make_unique<juce::Slider>();
-            sl->setSliderStyle(juce::Slider::LinearVertical);
-            sl->setTextBoxStyle(juce::Slider::NoTextBox, true, 0, 0);
-            background->addAndMakeVisible(*sl);
-            sl->setSize(8, 28);
-            sl->setMouseDragSensitivity(28);
-            sl->setRange(0, 1);
-            sl->setValue(0.5);
-            sl->setDoubleClickReturnValue(true, 0.5);
-            // TODO: Get the "snap to mouse position" to work with the scaling we have where we only
-            // use 90% of the track (the remaining 10% is for the bottom part of the thumb; the
-            // thumb's "position" is the very top pixel of the thumb). Until then, it doesn't work
-            // right throughout the whole track, so we set this to false.
-            sl->setSliderSnapsToMousePosition(false);
-            dx = 0.6875f;
-            dy = 0.6875f;
-            elements.push_back(std::move(sl));
-        }
-        else if (id.endsWith("cv") || id.endsWith("input") || id.endsWith("output"))
-        {
-            auto led = std::make_unique<sapphire::LedVu>();
-            background->addAndMakeVisible(*led);
-            led->setSize(3, 3);
-            dx = 0.5f;
-            dy = 0.5f;
-            elements.push_back(std::move(led));
-        }
-        else
-        {
-            continue;
-        }
-        juce::Point<float> real{cx + dx, cy + dy};
-        juce::Point<int> rounded = real.toInt();
-        elements.back()->setCentrePosition(rounded);
-        elements.back()->setTransform(juce::AffineTransform::translation(
-            real.getX() - rounded.getX(), real.getY() - rounded.getY()));
+        found.insert({id.toStdString(), {cx, cy}});
     }
+
+    float x, y;
+    // Add input tilt section.
+    std::tie(x, y) = found.at("input_tilt_atten");
+    tilt_in.atten = make_small_knob(x, y);
+    std::tie(x, y) = found.at("input_tilt_cv");
+    tilt_in.vu = make_led_vu(x, y);
+    std::tie(x, y) = found.at("input_tilt_knob");
+    tilt_in.slider = make_large_knob(x, y);
+    // Add output tilt section.
+    std::tie(x, y) = found.at("output_tilt_atten");
+    tilt_out.atten = make_small_knob(x, y);
+    std::tie(x, y) = found.at("output_tilt_cv");
+    tilt_out.vu = make_led_vu(x, y);
+    std::tie(x, y) = found.at("output_tilt_knob");
+    tilt_out.slider = make_large_knob(x, y);
+    // Add friction section.
+    std::tie(x, y) = found.at("fric_atten");
+    fric.atten = make_small_knob(x, y);
+    std::tie(x, y) = found.at("fric_cv");
+    fric.vu = make_led_vu(x, y);
+    std::tie(x, y) = found.at("fric_slider");
+    fric.slider = make_slider(x, y);
+    // Add stiffness section.
+    std::tie(x, y) = found.at("stif_atten");
+    stif.atten = make_small_knob(x, y);
+    std::tie(x, y) = found.at("stif_cv");
+    stif.vu = make_led_vu(x, y);
+    std::tie(x, y) = found.at("stif_slider");
+    stif.slider = make_slider(x, y);
+    // Add span section.
+    std::tie(x, y) = found.at("span_atten");
+    span.atten = make_small_knob(x, y);
+    std::tie(x, y) = found.at("span_cv");
+    span.vu = make_led_vu(x, y);
+    std::tie(x, y) = found.at("span_slider");
+    span.slider = make_slider(x, y);
+    // Add curl section.
+    std::tie(x, y) = found.at("curl_atten");
+    curl.atten = make_small_knob(x, y);
+    std::tie(x, y) = found.at("curl_cv");
+    curl.vu = make_led_vu(x, y);
+    std::tie(x, y) = found.at("curl_slider");
+    curl.slider = make_slider(x, y);
+    // Add mass section.
+    std::tie(x, y) = found.at("mass_atten");
+    mass.atten = make_small_knob(x, y);
+    std::tie(x, y) = found.at("mass_cv");
+    mass.vu = make_led_vu(x, y);
+    std::tie(x, y) = found.at("mass_slider");
+    mass.slider = make_slider(x, y);
+    // Input drive.
+    std::tie(x, y) = found.at("drive_knob");
+    in_drive = make_large_knob(x, y);
+    std::tie(x, y) = found.at("audio_left_input");
+    inl_vu = make_led_vu(x, y);
+    std::tie(x, y) = found.at("audio_right_input");
+    inr_vu = make_led_vu(x, y);
+    // Output level.
+    std::tie(x, y) = found.at("level_knob");
+    out_level = make_large_knob(x, y);
+    std::tie(x, y) = found.at("audio_left_output");
+    outl_vu = make_led_vu(x, y);
+    std::tie(x, y) = found.at("audio_right_output");
+    outr_vu = make_led_vu(x, y);
+    // Limiter warning light.
+    std::tie(x, y) = found.at("power_toggle");
+    limiter_warning = make_led_vu(x, y);
+    // Power toggle. FIXME: add.
+    std::tie(x, y) = found.at("power_gate_input");
 
 #if 0
       auto addKnob = [this](const std::string label, auto &paramRef) {
@@ -146,22 +175,69 @@ void ElastikaEditor::resized()
     {
         background->setTransformToFit(getLocalBounds().toFloat(), juce::RectanglePlacement());
     }
-#if 0
-    auto slsz = getWidth() / 4 * 0.8;
-    auto slmg = getWidth() / 4 * 0.1;
-    auto p = getLocalBounds().withTrimmedTop(80).withWidth(slsz).withHeight(slsz).translated(slmg, 0);
+}
 
-    for (int i=0; i<knobs.size(); ++i)
-    {
-        knobs[i]->setBounds(p);
-        auto l = p.translated(0, slsz + 2).withHeight(20);
-        labels[i]->setBounds(l);;
+std::unique_ptr<juce::Slider> ElastikaEditor::make_large_knob(float cx, float cy)
+{
+    static constexpr float dx = 0.5f;
+    static constexpr float dy = 0.5f;
+    auto kn = std::make_unique<juce::Slider>();
+    kn->setSliderStyle(juce::Slider::RotaryHorizontalVerticalDrag);
+    kn->setTextBoxStyle(juce::Slider::NoTextBox, true, 0, 0);
+    kn->setPopupMenuEnabled(true);
+    background->addAndMakeVisible(*kn);
+    kn->setSize(11, 11);
+    kn->setRange(0, 1);
+    kn->setValue(0.5);
+    kn->setDoubleClickReturnValue(true, 0.5);
+    set_control_position(*kn, cx, cy, dx, dy);
+    return kn;
+}
 
-        p = p.translated(slsz + 2 * slmg, 0);
+std::unique_ptr<juce::Slider> ElastikaEditor::make_small_knob(float cx, float cy)
+{
+    static constexpr float dx = 0.9166f;
+    static constexpr float dy = 0.9166f;
+    auto kn = std::make_unique<juce::Slider>();
+    kn->setSliderStyle(juce::Slider::RotaryHorizontalVerticalDrag);
+    kn->setTextBoxStyle(juce::Slider::NoTextBox, true, 0, 0);
+    kn->setPopupMenuEnabled(true);
+    background->addAndMakeVisible(*kn);
+    kn->setLookAndFeel(small_lnf.get());
+    kn->setSize(6, 6);
+    set_control_position(*kn, cx, cy, dx, dy);
+    return kn;
+}
 
+std::unique_ptr<sapphire::LedVu> ElastikaEditor::make_led_vu(float cx, float cy)
+{
+    static constexpr float dx = 0.5f;
+    static constexpr float dy = 0.5f;
+    auto led = std::make_unique<sapphire::LedVu>();
+    background->addAndMakeVisible(*led);
+    led->setSize(3, 3);
+    set_control_position(*led, cx, cy, dx, dy);
+    return led;
+}
 
-        if (i == 3)
-            p = getLocalBounds().withTrimmedTop(80 + slsz + 30).withWidth(slsz).withHeight(slsz).translated(slmg, 0);
-    }
-#endif
+std::unique_ptr<juce::Slider> ElastikaEditor::make_slider(float cx, float cy)
+{
+    static constexpr float dx = 0.6875f;
+    static constexpr float dy = 0.6875f;
+    auto sl = std::make_unique<juce::Slider>();
+    sl->setSliderStyle(juce::Slider::LinearVertical);
+    sl->setTextBoxStyle(juce::Slider::NoTextBox, true, 0, 0);
+    background->addAndMakeVisible(*sl);
+    sl->setSize(8, 28);
+    sl->setMouseDragSensitivity(28);
+    sl->setRange(0, 1);
+    sl->setValue(0.5);
+    sl->setDoubleClickReturnValue(true, 0.5);
+    // TODO: Get the "snap to mouse position" to work with the scaling we have where we only use 90%
+    // of the track (the remaining 10% is for the bottom part of the thumb; the thumb's "position"
+    // is the very top pixel of the thumb). Until then, it doesn't work right throughout the whole
+    // track, so we set this to false.
+    sl->setSliderSnapsToMousePosition(false);
+    set_control_position(*sl, cx, cy, dx, dy);
+    return sl;
 }
