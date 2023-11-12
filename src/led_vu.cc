38bcfcad68547a08654dad815ef50c08e76c1f0c
Morgon Kanter <morgon.kanter@gmail.com>
2023-11-12 17:04:39 -0500

    Use shadows to make the LEDs look a bit better.
    Also pick the colour used from a gradient, to show how close we are to
    the limits.

--- src/led_vu.cc
+++ src/led_vu.cc
@@ -3,24 +3,45 @@
 namespace sapphire
 {
 
-LedVu::LedVu() { setPaintingIsUnclipped(true); }
+LedVu::LedVu()
+{
+    setPaintingIsUnclipped(true);
+
+    led_picker_.addColour(0, good_col_);
+    led_picker_.addColour(0.8, good_col_);
+    led_picker_.addColour(1, limit_col_);
+    led_shadow_picker_.addColour(0, good_shadow_);
+    led_shadow_picker_.addColour(0.8, good_shadow_);
+    led_shadow_picker_.addColour(1, limit_shadow_);
+}
 
 void LedVu::paint(juce::Graphics &g)
 {
-    float diam = std::min<float>(getWidth(), getHeight()) - line_thickness_;
+    const float diam = std::min<float>(getWidth(), getHeight()) - line_thickness_;
+    const float bezel_diam = diam + line_thickness_;
     juce::Path p;
     p.addEllipse(0, 0, diam, diam);
 
-    juce::ColourGradient grad(juce::Colours::white, 0, 0, juce::Colours::green, diam, diam, true);
+    // FIXME
+    const double point = 0.92;
+
+    // To determine the color to be used, construct a gradient and pull from
+    // along it.
+    juce::Colour shadow_color;
+    juce::Colour led_color;
+    shadow_color = led_shadow_picker_.getColourAtPosition(point);
+    led_color = led_picker_.getColourAtPosition(point);
+
+    juce::DropShadow(shadow_color.withAlpha(0.7f), 1, {}).drawForPath(g, p);
+
+    juce::ColourGradient grad(juce::Colours::white, 0, 0, led_color, diam, diam, true);
     g.setGradientFill(grad);
     g.fillPath(p);
 
     // Add the outer line.
     g.setColour(outline_col_);
-    g.setOpacity(0.4f);
-    g.drawEllipse(0, 0, diam, diam, line_thickness_);
-
-    juce::DropShadow(juce::Colours::green.withAlpha(0.7f), 1, {}).drawForPath(g, p);
+    g.setOpacity(0.7f);
+    g.drawEllipse(0, 0, bezel_diam, bezel_diam, line_thickness_);
 }
 
 } // namespace sapphire
--- src/led_vu.h
+++ src/led_vu.h
@@ -13,10 +13,15 @@ class LedVu : public juce::Component
     void paint(juce::Graphics &g) override;
 
   private:
-    static constexpr float line_thickness_ = 0.2f;
-    const juce::Colour good_col_ = juce::Colour(0xFFABFF00);;
+    static constexpr float line_thickness_ = 0.25f;
+    const juce::Colour good_col_ = juce::Colours::green;
+    const juce::Colour good_shadow_ = juce::Colours::lightgreen;
     const juce::Colour limit_col_ = juce::Colours::red;
+    const juce::Colour limit_shadow_ = juce::Colours::mediumvioletred;
     const juce::Colour outline_col_ = juce::Colours::black;
+
+    juce::ColourGradient led_picker_;
+    juce::ColourGradient led_shadow_picker_;
 };
 
 } // namespace sapphire
