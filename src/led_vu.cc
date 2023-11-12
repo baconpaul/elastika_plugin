6cd454363241b971b918cdc247c40038939b3034
Morgon Kanter <morgon.kanter@gmail.com>
2023-11-11 21:57:04 -0500

    Make the LEDs look more like...LEDs.
    They still don't change color.

--- src/led_vu.cc
+++ src/led_vu.cc
@@ -3,10 +3,24 @@
 namespace sapphire
 {
 
+LedVu::LedVu() { setPaintingIsUnclipped(true); }
+
 void LedVu::paint(juce::Graphics &g)
 {
-    float diam = std::min<float>(getWidth(), getHeight()) - outerLine;
-    juce::LookAndFeel_V2::drawGlassSphere(g, 0, 0, diam, col, outerLine);
+    float diam = std::min<float>(getWidth(), getHeight()) - line_thickness_;
+    juce::Path p;
+    p.addEllipse(0, 0, diam, diam);
+
+    juce::ColourGradient grad(juce::Colours::white, 0, 0, juce::Colours::green, diam, diam, true);
+    g.setGradientFill(grad);
+    g.fillPath(p);
+
+    // Add the outer line.
+    g.setColour(outline_col_);
+    g.setOpacity(0.4f);
+    g.drawEllipse(0, 0, diam, diam, line_thickness_);
+
+    juce::DropShadow(juce::Colours::green.withAlpha(0.7f), 1, {}).drawForPath(g, p);
 }
 
 } // namespace sapphire
--- src/led_vu.h
+++ src/led_vu.h
@@ -8,11 +8,15 @@ namespace sapphire
 class LedVu : public juce::Component
 {
   public:
+    LedVu();
+
     void paint(juce::Graphics &g) override;
 
   private:
-    static constexpr float outerLine = 0.2f;
-    const juce::Colour col = juce::Colours::white;
+    static constexpr float line_thickness_ = 0.2f;
+    const juce::Colour good_col_ = juce::Colour(0xFFABFF00);;
+    const juce::Colour limit_col_ = juce::Colours::red;
+    const juce::Colour outline_col_ = juce::Colours::black;
 };
 
 } // namespace sapphire
